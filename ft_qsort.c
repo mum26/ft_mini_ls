3b42049e143b19fdf082d1ec8db088aa84013d41
石毛秀悟 <ishigeshuugo@shuugoea.local>
2024-08-02 09:01:09 +0900

    Mod ft_qsort.c

--- ft_qsort.c
+++ ft_qsort.c
@@ -1,10 +1,35 @@
 #include "ft_mini_ls.h"
 
+void	ft_swap(void *a, void *b, size_t width)
+{
+	void	*tmp;
+
+	tmp = malloc(width);
+	if (!tmp)
+	{
+		peeror("swap() -> malloc");
+		return ;
+	}
+	ft_memcpy(tmp, a, width);
+	ft_memcpy(a, b, width);
+	ft_memcpy(b, tmp, width);
+	free(tmp);
+}
+
 static void	do_qsort(unsigned char *base, size_t left, size_t right, size_t width, int (*compar)(const void *, const void *))
 {
 	if (left >= right)
 		return ;
-	size_t 
+	size_t p_index = left + (right - left) / 2;
+	unsigned char *pivot = base + p_index * width;
+	swap(base + p_index * width, base + right * width, width);
+
+	size_t	i = left;
+	size_t	j = left;
+	while (j < right)
+		if (compar(base + j * width, pivot) < 0)
+			swap(base + i++ * width, base + j++ * width, width);
+	swap()
 }
 
 void	ft_qsort(void *base, size_t nel, size_t width, int (*compar)(const *void)(const *void))
